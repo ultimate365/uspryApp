c49e47ea7cbca5bc7d718e0533c78f298d7f5018
amtawestwbtpta <110799204+amtawestwbtpta@users.noreply.github.com>
2025-09-15 11:59:29 +0530

    Remodified

--- src/screens/CreateHeroResult.jsx
+++ src/screens/CreateHeroResult.jsx
@@ -119,8 +119,8 @@ export default function CreateHeroResult() {
     { fullName: 'Bengali', shortName: 'ben' },
     { fullName: 'English', shortName: 'eng' },
     { fullName: 'Mathematics', shortName: 'math' },
-    { fullName: 'Health', shortName: 'health' },
     { fullName: 'Work Education', shortName: 'work' },
+    { fullName: 'Health', shortName: 'health' },
     { fullName: 'ENVS', shortName: 'envs' },
   ];
 
